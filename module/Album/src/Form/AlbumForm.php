815971c071a69fdde0decf795fe15e1d3c758eb1
Patrick Thuan <patrick.thuan@itl.com>
2018-04-02 17:53:45 +0700

    Update AlbumForm

--- module/Album/src/Form/AlbumForm.php
+++ module/Album/src/Form/AlbumForm.php
@@ -17,5 +17,29 @@ class AlbumForm extends Form {
             'name' => 'id',
             'type' => 'hidden',
         ]);
+        $this->add([
+            'name' => 'title',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Title'
+            ]
+        ]);
+        $this->add([
+            'name' => 'artist',
+            'type' => 'text',
+            'options' => [
+                'label' => 'Artist',
+            ]
+        ]);
+        $this->add([
+            'name' => 'submit',
+            'type' => 'submit',
+            'attributes' => [
+                'value' => 'Go',
+                'id' => 'submitbutton'
+            ]
+        ]);
+
+        $this->setAttribute('method',  'POST');
     }
 }
