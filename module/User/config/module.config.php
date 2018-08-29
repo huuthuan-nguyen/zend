7024cc52c1e90dba864db92a9579c73b4dfadfa3
Thuan Nguyen <huuthuan.nguyen@hotmail.com>
2018-08-29 17:06:07 +0700

    Update Authentication feature.

--- module/User/config/module.config.php
+++ module/User/config/module.config.php
@@ -1,5 +1,7 @@
 <?php
 namespace User;
+
+use Application\Controller\IndexController;
 use User\Service\Factory\AuthenticationServiceFactory;
 use Zend\Authentication\AuthenticationService;
 
@@ -13,6 +15,27 @@ return [
     'service_manager' => [
         'factories' => [
             AuthenticationService::class => AuthenticationServiceFactory::class
+        ]
+    ],
+    // The 'access_filter' key is used by module User to restrict or permit
+    // access to certain controller actions for unauthenticated visitors.
+    'access_filter' => [
+        'options' => [
+            // The access filter can work in 'restrictive' (recommended) or 'permissive'
+            // mode. In restrictive mode all controller actions must be explicitly listed
+            // under the 'access_filter' config key, and access is denied to any not listed
+            // action for users not logged in. In permissive mode, if an action is not listed
+            // under the 'access_filter' key, access to it is permitted to anyone (even for
+            // users not logged in. Restrictive mode is more secure and recommended.
+            'mode' => 'restrictive'
+        ],
+        'controllers' => [
+            IndexController::class => [
+                // Allow anyone to visit "index" and "about" actions
+                ['actions' => ['index', 'about'], 'allow' => '*'],
+                // Allow authenticated users to visit "settings" action
+                ['actions' => ['settings'], 'allow' => '@']
+            ]
         ]
     ]
 ];
--- module/User/src/Service/AuthManager.php
+++ module/User/src/Service/AuthManager.php
@@ -1,6 +1,8 @@
 <?php
 namespace User\Service;
 use Zend\Authentication\AuthenticationService;
+use Zend\Authentication\Result;
+use Zend\Session\SessionManager;
 
 /**
  * Created by PhpStorm.
@@ -12,14 +14,65 @@ use Zend\Authentication\AuthenticationService;
 class AuthManager {
 
     /**
+     * @var SessionManager
+     */
+    protected $sessionManager;
+
+    /**
      * @var AuthenticationService
      */
-    private $authService;
+    protected $authService;
 
+    /**
+     * AuthManager constructor.
+     * @param SessionManager $sessionManager
+     * @param AuthenticationService $authService
+     */
+    public function __construct(SessionManager $sessionManager, AuthenticationService $authService)
+    {
+        $this->sessionManager = $sessionManager;
+        $this->authService = $authService;
+    }
+
+    /**
+     * Performs a login attempt. If $rememberMe argument is true, if forces the session
+     * to last for one month (otherwise the session expires on one hour).
+     *
+     * @param $email
+     * @param $password
+     * @param $rememberMe
+     * @return Result
+     * @throws \Exception
+     */
     public function login($email, $password, $rememberMe) {
         if ($this->authService->getIdentity() != null)
             throw new \Exception('Already logged in');
 
         $authAdapter = $this->authService->getAdapter();
+        $authAdapter->setEmail($email);
+        $authAdapter->setPassword($password);
+        $result = $this->authService->authenticate();
+
+        // If user wants to "remember him", we will make session to expire in
+        // one month. By default session expires in 1 hour (as specific in our
+        // config/global.php file).
+        if ($result->getCode() == Result::SUCCESS && $rememberMe)
+            // Session cookie will expire in 1 month (30 days).
+            $this->sessionManager->rememberMe(60*60*24*30);
+
+        return $result;
+    }
+
+    /**
+     * Performs user logout.
+     * @throws \Exception
+     */
+    public function logout() {
+        // Allow to log out only when user is logged in.
+        if ($this->authService->getIdentity() == null)
+            throw new \Exception('The user is not logged in');
+
+        // Remove identity form session.
+        $this->authService->clearIdentity();
     }
 }
