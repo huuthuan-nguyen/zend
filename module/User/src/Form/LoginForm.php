93426694561956fe8c9852a6ef085b85b31de737
huuthuan-nguyen <33919309+huuthuan-nguyen@users.noreply.github.com>
2018-09-12 07:43:40 +0700

    Update LoginForm & PasswordChangeForm

--- module/User/src/Form/LoginForm.php
+++ module/User/src/Form/LoginForm.php
@@ -1,7 +1,167 @@
 <?php
+
+namespace User\Form;
+
+use Zend\Filter\StringTrim;
+use Zend\Form\Element\Checkbox;
+use Zend\Form\Element\Csrf;
+use Zend\Form\Element\Email;
+use Zend\Form\Element\Hidden;
+use Zend\Form\Element\Password;
+use Zend\Form\Element\Text;
+use Zend\Form\Form;
+use Zend\Validator\EmailAddress;
+use Zend\Validator\Hostname;
+use Zend\Validator\InArray;
+use Zend\Validator\StringLength;
+
 /**
- * Created by PhpStorm.
- * User: Thuan Nguyen
- * Date: 8/22/2018
- * Time: 10:30 AM
+ * Class LoginForm is used to collect user's login, password and 'Remember Me' flag.
+ * @package User\Form
  */
+class LoginForm extends Form
+{
+
+    public function __construct($name = null, array $options = [])
+    {
+        //Define form name.
+        $name = 'login-form';
+        parent::__construct($name, $options);
+
+        // Set POST method for this form.
+        $this->setAttribute('method', 'POST');
+
+        $this->addElements();
+        $this->addInputFilter();
+    }
+
+    /**
+     * This method adds elements to form (input filters and submit button).
+     */
+    public function addElements()
+    {
+        // Add "email" field.
+        $this->add([
+            'type' => Text::class,
+            'name' => 'email',
+            'options' => [
+                'label' => 'Your E-mail'
+            ]
+        ]);
+        // Add "password" field.
+        $this->add([
+            'type' => Password::class,
+            'name' => 'password',
+            'options' => [
+                'label' => 'Password'
+            ]
+        ]);
+        // Add "remember_me" field.
+        $this->add([
+            'type' => Checkbox::class,
+            'name' => 'remember_me',
+            'options' => [
+                'label' => 'Remember me'
+            ]
+        ]);
+        // Add "redirect_url" field.
+        $this->add([
+            'type' => Hidden::class,
+            'name' => 'redirect_url'
+        ]);
+        // Add the CSRF field.
+        $this->add([
+            'type' => Csrf::class,
+            'name' => 'csrf',
+            'options' => [
+                'csrf_option' => [
+                    'timeout' => 600
+                ]
+            ]
+        ]);
+        // Add the submit button.
+        $this->add([
+            'type' => 'submit',
+            'name' => 'submit',
+            'attributes' => [
+                'value' => 'Sign in',
+                'id' => 'submit'
+            ]
+        ]);
+    }
+
+    /**
+     * This method creates input filter (used for form filtering/validation).
+     */
+    public function addInputFilter() {
+        // Create main input filter.
+        $inputFilter = $this->getInputFilter();
+
+        // Add input for "email" field.
+        $inputFilter->add([
+            'name' => Email::class,
+            'required' => true,
+            'filters' => [
+                ['name' => StringTrim::class]
+            ],
+            'validators' => [
+                [
+                    'name' => EmailAddress::class,
+                    'options' => [
+                        'allow' => Hostname::ALLOW_DNS,
+                        'useMxCheck' => false
+                    ]
+                ]
+            ]
+        ]);
+
+        // Add input for "password" field.
+        $inputFilter->add([
+            'name' => Password::class,
+            'required' => true,
+            'filters' => [],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'min' => 6,
+                        'max' => 64
+                    ]
+                ]
+            ]
+        ]);
+
+        // Add input for "remember_me" field.
+        $inputFilter->add([
+            'name' => 'remember_me',
+            'required'=> false,
+            'filters' => [],
+            'validators' => [
+                [
+                    'name' => InArray::class,
+                    'options' => [
+                        'haystack' => [0, 1]
+                    ]
+                ]
+            ]
+        ]);
+
+        // Add input for "redirect_url" field.
+        $inputFilter->add([
+            'name' => 'redirect_url',
+            'required' => false,
+            'filters' => [
+                ['name' => StringTrim::class]
+            ],
+            'validators' => [
+                [
+                    'name' => StringLength::class,
+                    'options' => [
+                        'min' => 0,
+                        'max' => 2048
+                    ]
+                ]
+            ]
+        ]);
+    }
+}
--- module/User/src/Form/PasswordChangeForm.php
+++ module/User/src/Form/PasswordChangeForm.php
@@ -1,7 +1,93 @@
 <?php
+namespace User\Form;
+
+use Zend\Form\Element\Csrf;
+use Zend\Form\Element\Password;
+use Zend\Form\Element\Submit;
+use Zend\Form\Form;
+
 /**
- * Created by PhpStorm.
- * User: Thuan Nguyen
- * Date: 8/22/2018
- * Time: 10:30 AM
+ * Class PasswordChangeForm is used when changing user's password (to collect user's old password
+ * and new password) or when resetting user's password (when user forgot his password)
+ * @package User\Form
  */
+class PasswordChangeForm extends Form {
+    // There can be two scenarios - "change" or "reset"
+    private $scenario;
+
+    public function __construct($name = null, array $options = [])
+    {
+        // Define form name
+        $name = 'password-change-form';
+        parent::__construct($name, $options);
+
+        // Set POST method for this form.
+        $this->setAttribute('method', 'POST');
+
+        $this->addElements();
+        $this->addInputFilter();
+    }
+
+    /**
+     * This method adds elements to form (input field and submit button).
+     */
+    protected function addElements() {
+        // If scenario is "change", we do not ask for old password.
+        if ($this->scenario == 'change') {
+
+            // Add "old_password" field.
+            $this->add([
+                'type' => Password::class,
+                'name' => 'old_password',
+                'options' => [
+                    'label' => 'Old Password'
+                ]
+            ]);
+        }
+
+        // Add "new_password" field.
+        $this->add([
+            'type' => Password::class,
+            'name' => 'new_password',
+            'options' => [
+                'label' => 'New Password',
+            ]
+        ]);
+
+        // Add "confirm_new_password" field.
+        $this->add([
+            'type' => Password::class,
+            'name'=> 'confirm_new_password',
+            'options' => [
+                'label' => 'Confirm New Password'
+            ]
+        ]);
+
+        // Add CSRF field
+        $this->add([
+            'type' => Csrf::class,
+            'name' => 'csrf',
+            'options' => [
+                'csrf_options' => [
+                    'timeout' => 600
+                ]
+            ]
+        ]);
+
+        // Add the Submit button
+        $this->add([
+            'type' => Submit::class,
+            'name' => 'submit',
+            'attributes' => [
+                'value' => 'Change Password'
+            ]
+        ]);
+    }
+
+    /**
+     * This method creates input filter (used for form filtering/validation)
+     */
+    protected function addInputFilter() {
+        // Create main input filter.
+    }
+}
